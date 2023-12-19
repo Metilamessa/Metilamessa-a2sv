f72e0a4ef106fdceb8429fbf38bdda3199031444
Metilamessa <140429504+Metilamessa@users.noreply.github.com>
2023-12-19 14:26:25 +0300

    Added solution for Reverse String II

---
+++ week1/reverse-string-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        result = list(s)
+        for i in range(0, len(result), 2 * k):
+            result[i:i + k] = reversed(result[i:i + k])
+
+        return "".join(result)
+        
