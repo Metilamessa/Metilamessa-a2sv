18033a08ee530ef519a84745edb7eb949e2de05a
metilamessa <metalamessa02@gmail.com>
2024-02-27 22:15:45 +0300

    Add solution for Pascal's Triangle II

--- folder/leetcode/pascals-triangle-ii.py
+++ folder/leetcode/pascals-triangle-ii.py
@@ -1,19 +1,24 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        row = []
         if rowIndex == 0:
-             return [1]
-        if rowIndex == 1:
-               return [1, 1] 
-            
-        else:
-            prev_row = self.getRow(rowIndex - 1)
-            row.append(1)
-            for i in range(1, rowIndex):
-                row.append(prev_row[i - 1] + prev_row[i])
-            row.append(1)
-            return row
+            return [1]
 
+        def helper(index):
+
+            if index == 1:
+                return [1, 1]
+
+            arr = helper(index - 1)
+            
+            ans = [0] * (index + 1)
+            ans[0], ans[-1] = 1, 1
+
+            for i in range(1, len(ans) -1):
+                ans[i] = arr[i -1] + arr[i]
+
+            return ans
+
+        return helper(rowIndex)
 
 
 
